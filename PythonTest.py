e761915f880284e22db9aa4a3b9672685268f8ee
XA1981 <48088802+XA1981@users.noreply.github.com>
2019-04-05 15:12:14 +0530

    Update PythonTest.py

--- PythonTest.py
+++ PythonTest.py
@@ -1,6 +1,10 @@
 import pandas as pd 
 import xlrd
- 
+import numpy as np
+
 df = pd.read_excel("PythonTest.xlsx") 
 
 print(df.head())
+
+df['Name'] = np.where(df['Emp_Id']>105, 'Abhi', df['Name'])
+print(df)
